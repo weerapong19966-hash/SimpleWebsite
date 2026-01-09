c3701c44d8c44b60822cc194b8e73da29bd1a89a
weerapong19966-hash <weerapong19966@gmail.com>
2026-01-09 16:22:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+let users = JSON.parse(localStorage.getItem("users")) || [
+    { username: "admin", password: "1234", role: "admin" }
+];
+
 function register() {
     const user = regUser.value;
     const pass = regPass.value;
 
     if (!user || !pass) {
-        alert("Please fill all fields");
+        alert("Fill all fields");
         return;
     }
 
-    localStorage.setItem("user", user);
-    localStorage.setItem("pass", pass);
+    users.push({ username: user, password: pass, role: "user" });
+    localStorage.setItem("users", JSON.stringify(users));
     alert("Register success!");
     location.hash = "login";
 }
@@ -17,13 +21,20 @@ function login() {
     const user = loginUser.value;
     const pass = loginPass.value;
 
-    if (
-        user === localStorage.getItem("user") &&
-        pass === localStorage.getItem("pass")
-    ) {
-        localStorage.setItem("login", "true");
-        window.location.href = "dashboard.html";
+    const found = users.find(
+        u => u.username === user && u.password === pass
+    );
+
+    if (!found) {
+        alert("Invalid login");
+        return;
+    }
+
+    localStorage.setItem("loginUser", JSON.stringify(found));
+
+    if (found.role === "admin") {
+        window.location.href = "admin-dashboard.html";
     } else {
-        alert("Invalid username or password");
+        window.location.href = "user-dashboard.html";
     }
 }
